71498eff82a4268c590d89f2362cebe205b05c63
Malayev Yerkanat <153696190+yerkanat7@users.noreply.github.com>
2024-02-06 20:08:39 +0600

    Update Doctor.java 1

--- src/main/java/org/example/Doctor.java
+++ src/main/java/org/example/Doctor.java
@@ -10,4 +10,45 @@ public class Doctor extends Person implements Payable {
     public Doctor() {
         id = id_gen++;
     }
+
+     public Doctor(String name, String surname, int age, boolean gender, String specialization, int roomNumber, boolean isAvailable, double salary) {
+        super(name, surname, age, gender);
+        setSpecialization(specialization);
+        setRoomNumber(roomNumber);
+        setAvailable(isAvailable);
+        setSalary(salary);
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    
 }
