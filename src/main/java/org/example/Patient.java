145af7ad1456921cf152f088c7f1092b169bbcd2
hasdan <230547@astait.edu.kz>
2024-02-06 14:38:44 +0600

    Person and Patient

--- src/main/java/org/example/Patient.java
+++ src/main/java/org/example/Patient.java
@@ -1,4 +1,11 @@
 package org.example;
 
-public class Patient {
+public class Patient extends Person{
+    private String diagnosis;
+    private boolean isOnReecord;
+    private String bloodType;
+    private boolean isVaccinated;
+    private String chronicDiseases;
+    private boolean isInsurance;
+
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,5 +1,64 @@
 package org.example;
-
 public class Person {
-    
+    private int id;
+    private static int id_gen=1;
+    private String name;
+    private String surname;
+    private int age;
+    private boolean gender;
+    public Person(){
+        id=id_gen++;
+    }
+    public Person(String name, String surname, int age){
+        this();
+        setName(name);
+        setSurname(surname);
+    }
+    public void setName(String name){
+
+        this.name = name;
+    }
+    public void setSurname(String surname){
+
+        this.surname = surname;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getName(){
+
+        return surname;
+    }
+    public String getSurname(){
+
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public int getId(){
+
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
 }
+
