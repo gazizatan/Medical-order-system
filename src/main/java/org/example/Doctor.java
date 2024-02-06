12d7571a3e0007a58c77df8bdca0e54250c6e61d
Malayev Yerkanat <153696190+yerkanat7@users.noreply.github.com>
2024-02-06 19:53:29 +0600

    started creating class Doctor

--- src/main/java/org/example/Doctor.java
+++ src/main/java/org/example/Doctor.java
@@ -1,5 +1,13 @@
 package org.example;
+public class Doctor extends Person implements Payable {
+    private int id;
+    private static int id_gen = 1;
+    private String specialization;
+    private int roomNumber;
+    private boolean isAvailable;
+    private double salary;
 
-public class Doctor {
-
+    public Doctor() {
+        id = id_gen++;
+    }
 }
