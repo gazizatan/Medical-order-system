268a5ceeb25c820f63acd76bba112eca295e4fca
hasdan <230547@astait.edu.kz>
2024-02-18 20:19:08 +0600

    code optimization

--- src/main/java/org/example/Doctor.java
+++ src/main/java/org/example/Doctor.java
@@ -17,56 +17,6 @@ public class Doctor extends Person implements Payable {
     private boolean isAvailable;
     private double salary;
 
-
-
-     public Doctor(int id, String name, String surname, int age, boolean gender, String specialization, int roomNumber, boolean isAvailable, double salary) {
-        super(name, surname, age, gender);
-        setId(id);
-        setSpecialization(specialization);
-        setRoomNumber(roomNumber);
-        setAvailable(isAvailable);
-        setSalary(salary);
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-    
-    public void setSpecialization(String specialization) {
-        this.specialization = specialization;
-    }
-
-    public void setRoomNumber(int roomNumber) {
-        this.roomNumber = roomNumber;
-    }
-
-    public void setAvailable(boolean available) {
-        isAvailable = available;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getSpecialization() {
-        return specialization;
-    }
-
-    public int getRoomNumber() {
-        return roomNumber;
-    }
-
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
     
     @Override
     public double getPaymentAmount() {
--- src/main/java/org/example/Patient.java
+++ src/main/java/org/example/Patient.java
@@ -19,77 +19,9 @@ public class Patient extends Person implements Payable{
     private String chronicDiseases;
     private boolean isInsurance;
 
-    public Patient(int id, String name, String surname, int age, boolean gender, String diagnosis, boolean isOnReecord, boolean isVaccinated, String chronicDiseases, String bloodType){
-        super(name, surname, age, gender);
-        setId(id);
-        setBloodType(bloodType);
-        setChronicDiseases(chronicDiseases);
-        setDiagnosis(diagnosis);
-        setInsurance(isInsurance);
-        setOnReecord(isOnReecord);
-        setVaccinated(isVaccinated);
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-    
-    public void setDiagnosis(String diagnosis) {
-
-        this.diagnosis = diagnosis;
-    }
-
-    public void setBloodType(String bloodType) {
-        this.bloodType = bloodType;
-    }
-
-    public void setChronicDiseases(String chronicDiseases) {
-        this.chronicDiseases = chronicDiseases;
-    }
-
-    public void setInsurance(boolean insurance) {
-        isInsurance = insurance;
-    }
-
-    public void setVaccinated(boolean vaccinated) {
-        isVaccinated = vaccinated;
-    }
-
-    public void setOnReecord(boolean onReecord) {
-        isOnReecord = onReecord;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getBloodType() {
-        return bloodType;
-    }
-
-    public String getChronicDiseases() {
-        return chronicDiseases;
-    }
-
-    public String getDiagnosis() {
-        return diagnosis;
-    }
-
-    public boolean isVaccinated() {
-        return isVaccinated;
-    }
-
-    public boolean isInsurance() {
-        return isInsurance;
-    }
-
-    public boolean isOnReecord() {
-        return isOnReecord;
-    }
-
     @Override
     public double getPaymentAmount() {
-        if(isInsurance == false){
+        if(!isInsurance){
             return 20000;
         }
         return 0;
