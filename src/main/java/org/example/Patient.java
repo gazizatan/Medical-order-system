79a3295622ebf51089118e3326ff3ec8b4a56720
hasdan <230547@astait.edu.kz>
2024-02-18 22:40:28 +0600

    Adding and Implementing Referralable

--- src/main/java/org/example/Patient.java
+++ src/main/java/org/example/Patient.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashMap;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 
-public class Patient extends Person implements Payable{
+public class Patient extends Person implements Payable, Referralable {
     private int id;
     private String diagnosis;
     private boolean isOnReecord;
@@ -19,12 +21,24 @@ public class Patient extends Person implements Payable{
     private String chronicDiseases;
     private boolean isInsurance;
 
+
     @Override
     public double getPaymentAmount() {
         if(!isInsurance){
             return 20000;
         }
         return 0;
+    }
+    public void getReferral(String diagnosis){
+        HashMap<String, String> diagToDoc = new HashMap<String, String>();
+        diagToDoc.put("schizophrenia", "psychiatrist");
+        diagToDoc.put("personality disorders", "psychiatrist");
+        diagToDoc.put("Autism", "psychiatrist");
+        diagToDoc.put("sociophobia", "psychotherapist");
+        diagToDoc.put("ADHD", "psychotherapist");
+        diagToDoc.put("depression", "psychotherapist");
+        diagToDoc.put("neurosis", "neurologist");
+        System.out.println("You must go to the" + diagToDoc.get(diagnosis));
     }
     @Override
     public String toString() {
--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -1,51 +1,22 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Transaction {
     private int id;
     private Date date;
     private String description;
     private double amount;
 
-    public Transaction(int id, Date date, String description, double amount) {
-        this.id = id;
-        this.date = date;
-        this.description = description;
-        this.amount = amount;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
 
     @Override
     public String toString() {
